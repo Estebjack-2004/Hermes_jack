4a60bb64a9b9e97693001fa2704852168a594642
Jack Esteban Ortiz <ortizvasquezjuanesteban15@gmail.com>
2025-03-17 19:12:03 -0500

    first commit

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,138 @@
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Brand Logo -->
+        <a href="index3.html" class="brand-link">
+        <img src="vistas/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Hermes</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+            <img src="vistas/dist/img/red guy.jpeg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+            <a href="#" class="d-block">Red Guy</a>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+                with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="../layout/boxed.html" class="nav-link">
+                    <i class="fas fa-home"></i>
+                    <p>Inicio</p>
+                    </a>
+                </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="fas fa-user"></i>
+                <p>
+                    Usuarios
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="../../index.html" class="nav-link">
+                    <p>Usuarios</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../../index2.html" class="nav-link">
+                    <p>Permisos</p>
+                    </a>
+                </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="fas fa-desktop"></i>
+                <p>
+                    Equipos
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="../../index.html" class="nav-link">
+                    <p>Inventario</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../../index2.html" class="nav-link">
+                    <p>Recepción/Traspaso</p>
+                    </a>
+                </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="fas fa-file-alt"></i>
+                <p>
+                    Solicitudes
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="../../index.html" class="nav-link">
+                    <p>Reservas</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../../index2.html" class="nav-link">
+                    <p>Inmediatas</p>
+                    </a>
+                </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="fas fa-check-circle"></i>
+                <p>
+                    Autorizar
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="index.html" class="nav-link">
+                    <p>Autorizaciones</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index2.html" class="nav-link">
+                    <p>Solicitudes Vencidas</p>
+                    </a>
+                </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                    <a href="../layout/boxed.html" class="nav-link">
+                    <i class="fas fa-undo-alt"></i>
+                    <p>Devoluciones</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="docs/layout/boxed.html" class="nav-link">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <p>Salidas</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/layout/boxed.html" class="nav-link">
+                    <i class="fas fa-chart-bar"></i>
+                    <p>Reportes</p>
+                    </a>
+                </li>
+            </li>
+            </ul>
+        </nav>
+        </div>  
+    </aside>
